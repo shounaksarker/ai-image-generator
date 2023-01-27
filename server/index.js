56e7e047b6533b2e208c21c0ee95224350538a3a
shounaksarker <shounaksarker@gmail.com>
2023-01-27 16:39:13 +0600

    production -- 00

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json({limit: '50mb'}))
 app.use("/api/v1/dalle", dalleRoutes)
 
 app.get('/', async (req, res)=>{
-    res.send("Hello from DALL-E")
+    res.send("Hello from DALL-E");
 })
 
 const startServer = async ()=>{
